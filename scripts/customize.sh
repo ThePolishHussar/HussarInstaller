eecd7787c148f5128631bd6f1cb8664aef08c77a
ThePolishHussar <73184660+ThePolishHussar@users.noreply.github.com>
2022-05-20 20:59:51 -0400

    Added note for clarification

--- scripts/customize.sh
+++ scripts/customize.sh
@@ -1,4 +1,6 @@
 #! /bin/bash
+#this portion is highly personal prefrence and may conflict with many people user experience. either comment out this portion in the main script or use the defaults, i will not adjust this portion of the script (customize.sh & configs directory) based on feedback
+#IT IS BASED ENTIRLEY OFF OF MY PERSONAL PREF
 source $HOME/HussarInstaller/install.conf
 
 cp -r configs/home/.  /mnt/home/$username/
