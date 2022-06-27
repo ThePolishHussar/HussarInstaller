7731b51edb59b505c5d1f8b3d9afaf0aac309470
ThePolishHussar <73184660+ThePolishHussar@users.noreply.github.com>
2022-06-26 23:30:47 -0400

    Delete install.sh

--- scripts/install.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-source ./install.conf
-# 1. Pull Fastest mirror and enable parrallel downloads
-pacman -S --noconfirm reflector
-sed -i 37c"ParallelDownloads = ${parallelDL}" /etc/pacman.conf
-reflector -a 24 -c ${mirror} -f 7 -l 15 --sort rate --save /etc/pacman.d/mirrorlist
-pacstrap /mnt base linux linux-firmware base-devel grub efibootmgr nano networkmanager reflector $cpu-ucode $gpu
-genfstab -U /mnt > /mnt/etc/fstab
