0bfadfe829a77dd1399da25e20204ca288a6d638
ThePolishHussar <73184660+ThePolishHussar@users.noreply.github.com>
2022-07-01 17:04:09 -0400

    Corrected variable names

--- scripts/2-install.sh
+++ scripts/2-install.sh
@@ -2,8 +2,8 @@
 source ./install.conf
 
 pacman -S --noconfirm --needed reflector
-sed -i 37c"ParallelDownloads = ${parallelDL}" /etc/pacman.conf
-reflector -a 48 -c ${mirror} -f 15 -l 25 --sort rate --save /etc/pacman.d/mirrorlist
+sed -i 37c"ParallelDownloads = $PARALLEL_DL" /etc/pacman.conf
+reflector -a 48 -c "$MIRROR" -f 15 -l 25 --sort rate --save /etc/pacman.d/mirrorlist
 
 pacstrap /mnt \
 base base-devel linux linux-firmware \
@@ -11,13 +11,13 @@ grub efibootmgr \
 networkmanager 
 #pacman-contrib git reflector
 
-lscpu | grep GenuineIntel && cpu=intel-ucode
-lscpu | grep AuthenticAMD && cpu=amd-ucode
+lscpu | grep GenuineIntel && CPU=intel-ucode
+lscpu | grep AuthenticAMD && CPU=amd-ucode
 
-lspci | grep VGA | grep -E "Intel" && gpu="xf86-video-intel mesa"
-lspci | grep VGA | grep -E "NVIDIA|GeForce" && gpu="nvidia nvidia-utils"
-lspci | grep VGA | grep -E "Radeon|AMD" && gpu="xf86-video-amdgpu mesa"
+lspci | grep VGA | grep -E "Intel" && GPU="xf86-video-intel mesa"
+lspci | grep VGA | grep -E "NVIDIA|GeForce" && GPU="nvidia nvidia-utils"
+lspci | grep VGA | grep -E "Radeon|AMD" && GPU="xf86-video-amdgpu mesa"
 
-pacstap -S --noconfirm --needed $cpu $gpu
+pacstap -S --noconfirm --needed "$CPU" "$GPU"
 
 genfstab -U /mnt > /mnt/etc/fstab
